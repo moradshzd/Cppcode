dcae0b9f6bcbae961a5a4af3e1a8d3dbe1d9aa9d
moradshzd <118043449+moradshzd@users.noreply.github.com>
2022-11-24 22:50:37 +0100

    1.1

--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -3,18 +3,25 @@
 #include <sstream>
 
 int main (int argc,char **argv){
-    ros::init(argc , argv, "talker");
-    ros::NodeHandle nh;
+    ros::init(argc , argv, "talker"); /* initialize the input arguments and specify the node name */
+
+    ros::NodeHandle nh;               /* We use the NodeHandle as a reference for the node */
+
     ros::Publisher pub=nh.advertise<std_msgs::String>("chatter",1000);
+    /* first, call the publisher and put an object named pub in it and create topic through the NodeHandle and
+     *  advertise command, and we specify the type of our message, which is a string, and then we
+     *  specify the topic name and the queue size.*/
 
-    ros::Rate r(10);
+    ros::Rate r(10);/* We use this command to make the frequency of 10 Hz perform this operation*/
 
-    while(ros::ok()){
-        std_msgs::String msg;
-        msg.data="hello world ";
-        ROS_INFO("%s",msg.data.c_str());
-        pub.publish(msg);
+    while(ros::ok()){/*Run the following commands until the ros is ok*/
+        std_msgs::String msg;/* we create a String object called msg and this is the content of the topic*/
+        msg.data="hello world ";/*This is data of the message */
+        ROS_INFO("%s",msg.data.c_str());/*To display the message in ros, we convert the message to c_string*/
+        pub.publish(msg);/*With this command, the message will be published in the topic*/
         r.sleep();
+        /*  Because we did not use while in our code and the code is executed once, and if we want our command
+         *  to be executed until we stop it, we use the spin command.*/
         ;
     }
 
