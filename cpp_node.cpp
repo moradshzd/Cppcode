fd656c39fe6db9a51720fdc8cd728301028c5833
moradshzd <118043449+moradshzd@users.noreply.github.com>
2022-11-24 22:51:25 +0100

    1.2

--- cpp_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-
-int main (int argc , char **argv){
-    ros::init(argc , argv, "cpp_node");
-    ros::NodeHandle nh;
-    ROS_INFO("Node has started");
-    ros::Duration(1.0).sleep();
-    ROS_INFO("exited");
-}
