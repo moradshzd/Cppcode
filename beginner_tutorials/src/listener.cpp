f6d2797fb5f40da4fa5f1bd78ec1bcb8cae08a9d
moradshzd <118043449+moradshzd@users.noreply.github.com>
2022-11-24 22:48:48 +0100

    1.1

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -1,14 +1,22 @@
 #include "std_msgs/String.h"
 #include "ros/ros.h"
 
-void callback (const std_msgs::String &msg){
+void callback (const std_msgs::String &msg){      /*The goal is to get the content of the msg and display it*/
     ROS_INFO("i recived [%s]", msg.data.c_str());
+    /* We created a node that connects to the chatter topic and executes the callback function once for each
+     * message it receives from the chatter topic.*/
 }
 
 int main (int argc,char **argv){
-    ros::init(argc , argv, "listener");
-    ros::NodeHandle nh;
+    ros::init(argc , argv, "listener");/* initialize the input arguments and specify the node name */
+    ros::NodeHandle nh; /* We use the NodeHandle as a reference for the node */
+
     ros::Subscriber sub=nh.subscribe("chatter",1000, callback );
+    /*  call the subscriber and put an object named pub in it we call the topic through the NodeHandle and
+     *  subscribe command and then use callback function for using the msg or data of topic */
+
+
     ros::spin();
+    /*  Wherever we use (callback function), we use spin so that node can be executed repeatedly.*/
 
 }
