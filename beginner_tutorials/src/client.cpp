1c8ca8094c0004a957982fdce754f1220fd0473a
Leopold Palomo-Avellaneda <leopold.palomo@upc.edu>
2022-11-25 08:56:16 +0100

    Some fixes

--- beginner_tutorials/src/client.cpp
+++ beginner_tutorials/src/client.cpp
@@ -8,16 +8,19 @@
 int main (int argc,char **argv){
     ros::init(argc , argv, "cpp_client");/* initialize the input arguments and specify the node name */
     ros::NodeHandle nh;/* We use the NodeHandle as a reference for the node */
-    ros::ServiceClient client=nh.ServiceClient<beginner_tutorials::AddTwoInts>("/add_two_ints");
+    ros::ServiceClient client=nh.serviceClient<beginner_tutorials::AddTwoInts>("/add_two_ints");
     /*  Our goal is to create a client through the ServiceClient command, whose task is to send a request
      *  to the /add_two_ints server and receive the response.*/
-    beginner_tutorials::AddTwoInts srv;
+    beginner_tutorials::AddTwoInts::Request req; // check ho do you declare!!!!
+    beginner_tutorials::AddTwoInts::Reponse resp;
+    
     /*We call the AddTwoInts service from the beginner_tutorials package and put it in the srv object*/
-    srv.req.a=3;
-    srv.req.b=5;
-
-    if (client.call(srv))  {/**/
-        ROS_INFO("server answered : %d",(int) srv.res.sum) ;
+    req.a=3;
+    req.b=5;
+    // miss the waitForService
+    // 
+    if (client.call(req))  {/**/
+        ROS_INFO("server answered : %d",(int) srv.sum) ;
         }
             else {
             ROS_WARN("failed");
