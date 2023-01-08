db92392c2088b3886475a3e869c362d0238d5620
moradshzd <118043449+moradshzd@users.noreply.github.com>
2023-01-08 14:18:56 +0100

    Update cpp_node.cpp
    1.2

--- beginner_tutorials/src/cpp_node.cpp
+++ beginner_tutorials/src/cpp_node.cpp
@@ -2,10 +2,10 @@
 #include <ros/ros.h>
 
 int main (int argc , char **argv){
-    ros::init(argc , argv, "cpp_node");/* initialize the input arguments and specify the node name*/
-    ros::NodeHandle nh; /*We use the NodeHandle as a reference for the node */
-    ROS_INFO("Node has started");/*this command print "Node has started"*/
-    ros::Duration(1.0).sleep();/* pause time with Duration and sleep command*/
+    ros::init(argc , argv, "cpp_node");// initialize the input arguments and specify the node name
+    ros::NodeHandle nh; //We use the NodeHandle as a reference for the node 
+    ROS_INFO("Node has started");//this command print "Node has started"
+    ros::Duration(1.0).sleep();// pause time with Duration and sleep command
     ROS_INFO("exited");
 }
 
