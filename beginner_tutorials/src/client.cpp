3dbe35337ae4fb14eb08e51bd992df269a5bf009
moradshzd <118043449+moradshzd@users.noreply.github.com>
2023-01-08 14:17:53 +0100

    1.1

--- beginner_tutorials/src/client.cpp
+++ beginner_tutorials/src/client.cpp
@@ -3,13 +3,13 @@
 
 
 int main (int argc,char **argv){
-    ros::init(argc , argv, "client");/* initialize the input arguments and specify the node name */
-    ros::NodeHandle nh;/* We use the NodeHandle as a reference for the node */
+    ros::init(argc , argv, "client");// initialize the input arguments and specify the node name 
+    ros::NodeHandle nh;// We use the NodeHandle as a reference for the node 
     ros::ServiceClient client=nh.serviceClient<beginner_tutorials::AddTwoInts>("/add_two_ints");
     /*  Our goal is to create a client through the ServiceClient command, whose task is to send a request
      *  to the /add_two_ints server and receive the response.*/
     beginner_tutorials::AddTwoInts srv;
-    /*We call the AddTwoInts service from the beginner_tutorials package and put it in the srv object*/
+    //We call the AddTwoInts service from the beginner_tutorials package and put it in the srv object
     srv.request.a=3;
     srv.request.b=5;
 
