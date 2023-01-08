3ede01bfc952bf73b2b5c4efd99831ae10467cb3
moradshzd <118043449+moradshzd@users.noreply.github.com>
2023-01-08 14:19:47 +0100

    1.2

--- beginner_tutorials/src/cpp_node.cpp
+++ beginner_tutorials/src/cpp_node.cpp
@@ -1,7 +1,8 @@
 
 #include <ros/ros.h>
 
-int main (int argc , char **argv){
+int main (int argc , char **argv)
+{
     ros::init(argc , argv, "cpp_node");// initialize the input arguments and specify the node name
     ros::NodeHandle nh; //We use the NodeHandle as a reference for the node 
     ROS_INFO("Node has started");//this command print "Node has started"
